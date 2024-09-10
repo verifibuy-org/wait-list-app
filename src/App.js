e013eaaef464e449bf18555cd556249b43cf4dd3
Kwasi O-T <kwasi@kudigo.com>
2024-09-10 22:44:52 +0000

    imported logo, favicon and set up site structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import logo from './Logo-white.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content-container">
+          <div className="logo-container">
+              <img src={logo} alt="Kokorko Logo" />
+          </div>
+          <div className="content">
+              <div className="text-container">
+                  <h1>Kokorko Marketplace is Launching Soon</h1>
+                  <p>
+                      Be the first to experience Kokorko Marketplace! <br/> Sign up now with your phone number or email to stay updated and get exclusive early access
+                  </p>
+              </div>
+              <div className="form-container">
+                  <a href="https://form.jotform.com/242534443859060" target="_blank" rel='noopener noreferrer' class="btn">I'm in</a>
+              </div>
+          </div>
+      </div>
     </div>
   );
 }
