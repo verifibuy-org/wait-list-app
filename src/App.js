726abd76ba92cffebf963f7d7a1c17e816b0cdeb
Kwasi O-T <kwasi@kudigo.com>
2024-09-12 23:09:16 +0000

    created assets folder and added images

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import logo from './Logo-white.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="content-container">
-          <div className="logo-container">
-              <img src={logo} alt="Kokorko Logo" />
-          </div>
-          <div class="ball b1"></div>
-          <div className="content">
-              <div className="text-container">
-                  <h1>Kokorko Marketplace is Launching Soon</h1>
-                  <p>
-                      Be the first to experience Kokorko Marketplace! <br/> Sign up now with your phone number or email to stay updated and get exclusive early access
-                  </p>
-              </div>
-              <div class="ball b2"></div>
-              <div className="button-container">
-                  <a href="https://form.jotform.com/242534443859060" target="_blank" rel='noopener noreferrer' class="btn">I'm in</a>
-              </div>
-          </div>
-      </div> 
-  );
+    return (
+        <div className="content-container">
+            <div className="logo-container">
+                <img src={logo} alt="Kokorko Logo" />
+            </div>
+            <div class="ball b1"></div>
+            <div className="content">
+                <div className="text-container">
+                    <h1>Kokorko Marketplace is Launching Soon</h1>
+                    <p>
+                        Be the first to experience Kokorko Marketplace! <br/> Sign up now with your phone number or email to stay updated and get exclusive early access
+                    </p>
+                </div>
+                <div class="ball b2"></div>
+                <div className="button-container">
+                    <a href="https://form.jotform.com/242534443859060" target="_blank" rel='noopener noreferrer' class="btn">I'm in</a>
+                </div>
+            </div>
+        </div> 
+    );
 }
 
 export default App;
