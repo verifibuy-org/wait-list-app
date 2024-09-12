dbb6c3e760b5eab44bbb579677a92b0040a151cd
Kwasi O-T <kwasi@kudigo.com>
2024-09-12 23:59:09 +0000

    changed logo img and app structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './Logo-white.svg';
+import logo from './assets/Footer Logo.svg';
 import './App.css';
+import phoneImage from './assets/android-mobile.png'
 
 function App() {
     return (
@@ -7,15 +8,18 @@ function App() {
             <div className="logo-container">
                 <img src={logo} alt="Kokorko Logo" />
             </div>
-            <div class="ball b1"></div>
+            {/* <div class="ball b1"></div> */}
             <div className="content">
-                <div className="text-container">
-                    <h1>Kokorko Marketplace is Launching Soon</h1>
-                    <p>
-                        Be the first to experience Kokorko Marketplace! <br/> Sign up now with your phone number or email to stay updated and get exclusive early access
-                    </p>
+                <div className='text-container'>
+                    <div className='text'>
+                        <h1>Ready to shop and sell with zero stress?</h1>
+                        <p></p>
+                    </div>
+                    <div className='image-container'>
+                        <img src={phoneImage} alt="Phone" />
+                    </div>
                 </div>
-                <div class="ball b2"></div>
+                {/* <div class="ball b2"></div> */}
                 <div className="button-container">
                     <a href="https://form.jotform.com/242534443859060" target="_blank" rel='noopener noreferrer' class="btn">I'm in</a>
                 </div>
